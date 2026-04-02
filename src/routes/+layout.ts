7972e3ef21a3e0b9e2e217e3ff6d2ac722721bfc
Ghastrod <clement.jallat@centrale-med.fr>
2026-04-02 17:33:29 +0200

    Initial commit

---
+++ src/routes/+layout.ts
@@ -0,0 +1,1 @@
+const spa = true;
---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    const date = new Date();
+    const debutFatour = new Date("2026-04-17");
+    // Calculer la différence en millisecondes
+    const diff = debutFatour.getTime() - date.getTime();
+    // Convertir la différence en jours
+    const days = Math.ceil(diff / (1000 * 60 * 60 * 24));
+    // Afficher le résultat et le centrer au milieu de la page
+    const resultElement = document.getElementById("result");
+    resultElement.textContent = `Il reste ${days} jours avant le début de la Fatour.`;
+</script>
+
+<div id="result"></div>
