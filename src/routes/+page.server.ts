a862ffce62334fcd33191b274e34210f3dad825e
Clément JALLAT <clement.jallat@cvegroup.com>
2026-04-30 16:59:42 -0400

    Nouvelle date et bientot ajout du roadbook

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -5,11 +5,14 @@ export const load: PageServerLoad = () => {
     const now = new Date();
     const hour = now.getHours();
     const minute = now.getMinutes();
-    const fatourStart = new Date("2026-04-17");
+    const fatourStart = new Date("2027-04-23T00:00:00");
+    const fatourEnd = new Date("2027-05-02T23:59:59");
+    const isFatour = now >= fatourStart && now <= fatourEnd;
     const diff = fatourStart.getTime() - now.getTime();
     const daysUntilFatour = Math.ceil(diff / (1000 * 60 * 60 * 24));
 
     return {
+        isFatour,
         daysUntilFatour,
         message: getMessageForHour(hour, minute),
     };
